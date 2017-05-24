9d051a608694afee8ec05acf5499ae3aacdcde49
zocalus <zocalus.app@gmail.com>
2017-05-23 22:03:43 -0400

    Create book views form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,20 @@
 class BooksController < ApplicationController
+
 	def index
 	end
+
+    def new
+    	@book = Book.new
+    end
+
+    def create
+    	@book = Book.new(book_params)
+    end
+
+    private
+
+     def book_params
+        params.require(:book).permit(:title, :description, :author)
+     end
+
 end
